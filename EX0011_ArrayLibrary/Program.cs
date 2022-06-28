c47277e4081cc1bbd524bb9bd1dfb5b508d22262
Katerina <oeu89@mail.ru>
2022-06-28 20:15:42 +0300

    Add ex01arraylibrary

---
+++ EX0011_ArrayLibrary/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(int[] collection) //Метод заполняющий массив
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+
+
+}
+
+void PrintArray(int[] col) //Метод распечатывающий массив
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];  // Определили массив из 10 элементов
+
+FillArray(array);  //Вызов метода, заполняющего массив
+PrintArray(array); //Вызов метода, распечатывающего массив
